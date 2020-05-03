65aadc67bf1dce529324506769d6443316cf7c5e
CHAU Céline <littlecelinec@gmail.com>
2020-05-03 17:49:49 +0200

    Add structure of code

---
+++ ia-projet.py
@@ -0,0 +1,17 @@
+# Retrieve random position_sample row from dataset
+
+# Algo génétique : find p1-p6, first values = 1
+# mutation
+# croisement
+
+# Check with fitness function
+
+# Sort from fitness
+
+# Store solution
+
+# Use p1-p6 for the rest of position samples 
+
+# Store difference between sample and calculated x and y
+
+# Find the more accurate value 
