bdd4607ffbf2921b6ded9ff919318e17ad52c330
CHAU Céline <littlecelinec@gmail.com>
2020-05-03 20:00:02 +0200

    update code

--- ia-projet.py
+++ ia-projet.py
@@ -1,10 +1,12 @@
 import random
+import pandas as pd
 
 class Individu:
-    def __init__(self, t):
+    def __init__(self, t, x, y):
         self.t = t
         self.params = [random.randint(-10,10) for i in range(6)]
         self.x, self.y = self.calcul_pos()
+        self.dist = self.fitness(x, y)
         
     # Check if queen has conflict
     def calcul_pos(self):
@@ -19,18 +21,31 @@ class Individu:
         return np.sqrt(np.sum((Y - X)**2))
 
 
-# Retrieve random position_sample row from dataset
+def main(csv_path):
+	df = pd.read_csv(csv_path)
 
-# Algo génétique : generate individus
-# mutation
-# croisement
+	# Retrieve random position_sample row from dataset
+	position = df.ix[random.sample(x.index, n)]
+	return position;
+	# Algo génétique : generate individus
+	# mutation
+	# croisement
 
-# Check individus with fitness function
+	# Check individus with fitness function
 
-# Store individus
+	# Store individus
 
-# Use p1-p6 for the rest of position samples 
+	# Use p1-p6 for the rest of position samples 
 
-# Store difference between sample and calculated individus
+	# Store distance between sample and calculated individus
 
-# Find the more accurate value 
+	# Find the more accurate value 
+
+
+if __name__ == "__main__":
+    # test()
+ 
+    print(main("position_sample.csv"))
+ 
+    # allSols = algoLoop()
+    # print(*allSols, sep="\n")
