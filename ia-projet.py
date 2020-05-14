a38c4d44850a1ef3e02765f66e734141afb73cd9
CJ <cj.donato10@gmail.com>
2020-05-15 00:18:43 +0200

    Modified fitness and populations

--- ia-projet.py
+++ ia-projet.py
@@ -1,52 +1,50 @@
-import random
+import random, math, time
 import pandas as pd
-import math
 
 class Individu:
     def __init__(self, params=None):
         self.params = params if params else [random.uniform(-10, 10) for i in range(6)]
         self.dist = 0
 
+    def __str__(self):
+        params = "\n ".join("p" + str(ind+1) + " : " + str(el) for ind, el in enumerate(self.params))
+        return params + "\nDistance: " + str(self.dist)
+
     # Calculate position based on time (with formula)
     def calcul_pos(self, t):
         self.x = self.params[0] * math.sin(self.params[1] * t + self.params[2])
         self.y = self.params[3] * math.sin(self.params[4] * t + self.params[5])
 
+    # Calculate fitness of individu (median or average distance)
+    def fitness(self, data_arr, type="mediane"):
+        if type == "moyenne":
+            moy = 0
+            for ind, el in enumerate(data_arr):
+                self.calcul_pos(el[0])
+                moy += self.euc_dist(el[1], el[2])
+            self.dist = moy / len(data_arr)
+        elif type == "mediane":
+            med_list = list()
+            for ind, el in enumerate(data_arr):
+                self.calcul_pos(el[0])
+                med_list.append(self.euc_dist(el[1], el[2]))
+            med_list.sort()
+            med_ind = int(math.ceil(len(med_list) / 2))
+            self.dist = med_list[med_ind]
+
     # Calculate euclidean distance between individual pos and data pos
-    def fitness(self, t, x, y):
-        self.calcul_pos(t)
+    def euc_dist(self, x, y):
         res = math.pow(self.x - x, 2) + math.pow(self.y - y, 2)
         return math.sqrt(res)
 
     # Evaluate individual as a solution for data set based on threshold
     def isGood(self, threshold=5):
         return self.dist < threshold
-    
-    def __str__(self):
-        params = "\n ".join("p" + str(ind+1) + " : " + str(el) for ind, el in enumerate(self.params))
-        return params + "\nDistance: " + str(self.dist)
-    
-    def displayPop(pop):
-        for ind in pop:
-            ind.toString()
 
     # Type "moyenne" ou "mediane"
     def evaluate(pop, data_arr, type="moyenne"):
-        if type == "moyenne":
-            for individu in pop:
-                moy = 0
-                for ind, el in enumerate(data_arr):
-                    moy += individu.fitness(el[0], el[1], el[2])
-                individu.dist = moy / len(data_arr)
-        elif type == "mediane":
-            for individu in pop:
-                med_list = list()
-                for ind, el in enumerate(data_arr):
-                    med_list.append(individu.fitness(el[0], el[1], el[2]))
-                med_list.sort()
-                med_ind = int(math.ceil(len(med_list) / 2))
-                individu.dist = med_list[med_ind]
-
+        for individu in pop:
+            individu.fitness(data_arr, type)
         return sorted(pop, key=lambda x: x.dist)
     
     # Return list of individus
@@ -63,7 +61,7 @@ class Individu:
 
     # Return crossing individual
     def croisement(ind1, ind2):
-        return Individu(ind1.params[:3] + ind2.params[3:]), Individu(ind1.params[:3] + ind2.params[3:])
+        return Individu(ind1.params[:3] + ind2.params[3:]), Individu(ind2.params[:3] + ind1.params[3:])
 
     # Return sublist
     def selection(pop, ub, lb):
@@ -85,33 +83,51 @@ def main():
     df = pd.read_csv(csv_path, sep=";")
     data = df.to_numpy()
 
+    # Initialize variables and population
+    print("Searching for solution...")
+    pop = Individu.create_pop(100)
+    type = "mediane"  # ou moyenne
+    threshold = 0.1
     nbIterations =  0
     solutionTrouvee = False
-    pop = Individu.create_pop(25)
-    
-    print("Calculating...")
-    
+
     while not solutionTrouvee:
         nbIterations += 1
-        # evaluation = Individu.evaluate(pop, data, type="moyenne")
-        evaluation = Individu.evaluate(pop, data, type="mediane")
-        if evaluation[0].isGood(threshold=0.1):
+        evaluation = Individu.evaluate(pop, data, type=type)
+        if evaluation[0].isGood(threshold=threshold):
             solutionTrouvee = True
         else :
-            select = Individu.selection(evaluation, 10, 4)
+            # Selection
+            select = Individu.selection(evaluation, 75, 25)
+            # Croisement
             croises = []
             for i in range(0, len(select), 2):
                 croises += Individu.croisement(select[i], select[i + 1])
+            # Mutation
             mutes = []
             for s in select:
                 mutes.append(s.mutation())
-            # Create new pop
-            newAlea = Individu.create_pop(5)
+            # Nouvelle population
+            newAlea = Individu.create_pop(100)
             pop = select[:] + croises[:] + mutes[:] + newAlea[:]
 
     # Solution found
     print("Solution found:\n", evaluation[0], "\nNombre d'iterations: ", nbIterations)
-    Individu.calculate(data, ind=evaluation[0])
+    evals = [round(p, 2) for p in evaluation[0].params]
+    Individu.calculate(data, params=evals)
+
+    # Format noms et solution
+    et = ["Aumand", "Chau", "Donato"]
+    fileName = "_".join(et)
+    sol = ";".join(str(param) for param in evals)
+
+    # Ecriture dans un fichier
+    f = open(fileName + ".txt", "w")
+    f.write(sol)
+    f.close()
+    print(fileName, sol, sep="\n")
 
 if __name__ == "__main__":
+    start_time = time.time()
     main()
+    print("--- %s seconds ---" % (time.time() - start_time))
